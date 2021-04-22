cecfd1dcb7166de25671a639326d1489ad3b3146
Sean Sundberg <seansund@us.ibm.com>
2021-04-21 19:59:26 -0500

    Adds label input and crn, location, service, and label outputs (#12)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,26 @@ output "name" {
   depends_on  = [ibm_resource_instance.hpcs_instance]
   description = "The name of the provisioned HPCS instance."
 }
+
+output "crn" {
+  description = "The id of the provisioned redis instance"
+  value       = data.ibm_resource_instance.hpcs_instance.id
+}
+
+output "location" {
+  description = "The location of the provisioned redis instance"
+  value       = var.region
+  depends_on  = [data.ibm_resource_instance.hpcs_instance]
+}
+
+output "service" {
+  description = "The name of the key provisioned for the redis instance"
+  value       = local.service
+  depends_on = [data.ibm_resource_instance.hpcs_instance]
+}
+
+output "label" {
+  description = "The label for the redis instance"
+  value       = var.label
+  depends_on = [data.ibm_resource_instance.hpcs_instance]
+}
