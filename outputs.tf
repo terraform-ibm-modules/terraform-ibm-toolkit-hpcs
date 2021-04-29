fbc77c5a661d3359fa3aea45019c6ce403e49634
Sean Sundberg <seansund@us.ibm.com>
2021-04-28 22:35:34 -0500

    Adds guid to output variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "id" {
   description = "The id of the provisioned HPCS instance."
 }
 
+output "guid" {
+  value       = data.ibm_resource_instance.hpcs_instance.guid
+  description = "The id of the provisioned HPCS instance."
+}
+
 output "name" {
   value       = local.name
   depends_on  = [ibm_resource_instance.hpcs_instance]
@@ -10,18 +15,18 @@ output "name" {
 }
 
 output "crn" {
-  description = "The id of the provisioned redis instance"
+  description = "The id of the provisioned HPCS instance"
   value       = data.ibm_resource_instance.hpcs_instance.id
 }
 
 output "location" {
-  description = "The location of the provisioned redis instance"
+  description = "The location of the provisioned HPCS instance"
   value       = var.region
   depends_on  = [data.ibm_resource_instance.hpcs_instance]
 }
 
 output "service" {
-  description = "The name of the key provisioned for the redis instance"
+  description = "The name of the key provisioned for the HPCS instance"
   value       = local.service
   depends_on = [data.ibm_resource_instance.hpcs_instance]
 }
