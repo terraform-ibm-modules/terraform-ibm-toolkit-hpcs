79ea1967f1de265ce1e81b11351847149a6721a8
Sean Sundberg <seansund@us.ibm.com>
2021-08-20 06:54:08 -0500

    Adds variable to skip all the resources
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-hpcs.tf
+++ test/stages/stage2-hpcs.tf
@@ -4,7 +4,7 @@ module "dev_hpcs" {
   resource_group_name      = module.resource_group.name
   region                   = var.region
   name_prefix              = var.name_prefix
-  provision                = true
+  provision                = false
   ibmcloud_api_key         = var.ibmcloud_api_key
   number_of_crypto_units   = 2
 }
